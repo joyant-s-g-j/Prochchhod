e833de3ce42f0b5cbc26103957d96d666cd8bb59
joyantgupta <joyantsheikharguptajoy@gmail.com>
2025-04-28 19:55:12 +0600

    added motion on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,41 +4,48 @@ import { FileText } from 'lucide-react'
 import { Link } from 'react-router-dom'
 import DesktopNavbar from './DesktopNavbar'
 import MobileNavbar from './MobileNavbar'
+import { motion } from 'framer-motion'
 
 const Navbar = () => {
   return (
-    <Box 
-        as="nav"
-        position="fixed"
-        top="0"
-        margin="10px"
-        width="calc(100% - 20px)"   
-        zIndex="50"
-        border="2px #ccc solid"
-        bg="#ded7d0"
-        borderRadius="4xl"
-        backdropFilter="blur(70px)"
+    <motion.div
+        initial={{ opacity: 0, y:-50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{duration: 0.9}}
     >
-        <Box maxW="7xl" mx="auto" px={4}>
-            <Flex align="center" gap={5} h="16" justify="space-between">
-                <Flex align="center">
-                    <Link to="/">
-                        <Flex direction="row">
-                            <Icon as={FileText} boxSize={{base: "9", lg: "12"}} />
-                            <Text 
-                                fontSize={{base: "2xl", lg: "4xl"}} 
-                                style={{ fontFamily: "'Tangerine', cursive", fontWeight: "900" }}
-                            >
-                                Prochchhod
-                            </Text>
-                        </Flex>
-                    </Link>
+        <Box 
+            as="nav"
+            position="fixed"
+            top="0"
+            margin="10px"
+            width="calc(100% - 20px)"   
+            zIndex="50"
+            border="2px #ccc solid"
+            bg="#ded7d0"
+            borderRadius="4xl"
+            backdropFilter="blur(70px)"
+        >   
+            <Box maxW="7xl" mx="auto" px={4}>
+                <Flex align="center" gap={5} h="16" justify="space-between">
+                    <Flex align="center">
+                        <Link to="/">
+                            <Flex direction="row">
+                                <Icon as={FileText} boxSize={{base: "9", lg: "12"}} />
+                                <Text 
+                                    fontSize={{base: "2xl", lg: "4xl"}} 
+                                    style={{ fontFamily: "'Tangerine', cursive", fontWeight: "900" }}
+                                >
+                                    Prochchhod
+                                </Text>
+                            </Flex>
+                        </Link>
+                    </Flex>
+                    <DesktopNavbar />
+                    <MobileNavbar />
                 </Flex>
-                <DesktopNavbar />
-                <MobileNavbar />
-            </Flex>
+            </Box>
         </Box>
-    </Box>
+    </motion.div> 
   )
 }
 
